cb530e9e3cf43c5ce29d30893e10c7185ef52d57
yyysolhhh <solbaram37@gmail.com>
2025-02-25 11:50:32 +0900

    ch04-04 프로젝트 구현 2
     할 일 조회 API 기능 구현

--- yyysolhhh/todo-server/src/main/java/org/example/persist/TaskRepository.java
+++ yyysolhhh/todo-server/src/main/java/org/example/persist/TaskRepository.java
@@ -9,8 +9,8 @@ import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> { //JpaRepository - 상속받은 인터페이스에서 메서드 만들면 메서드 이름 규칙에 따라 쿼리를 생성해주는 기능 제공
 
-    List<TaskEntity> findAllByDueDate(Date dueDate);
+    List<TaskEntity> findAllByDueDate(Date dueDate); //TODO 왜 하나만 나오지?
     List<TaskEntity> findAllByStatus(TaskStatus status);
 }
--- yyysolhhh/todo-server/src/main/java/org/example/web/TaskController.java
+++ yyysolhhh/todo-server/src/main/java/org/example/web/TaskController.java
@@ -2,14 +2,16 @@ package org.example.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.constants.TaskStatus;
 import org.example.model.Task;
 import org.example.service.TaskService;
 import org.example.web.vo.TaskRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -21,6 +23,7 @@ public class TaskController {
 
     /**
      * 새로운 할 일 추가
+     *
      * @param req 추가하고자 하는 할 일
      * @return 추가된 할 일
      */
@@ -28,5 +31,46 @@ public class TaskController {
     public ResponseEntity<Task> createTask(@RequestBody TaskRequest req) {
         var result = this.taskService.add(req.getTitle(), req.getDescription(), req.getDueDate()); //TODO var 왜 쓰는거지?
         return ResponseEntity.ok(result); //http 응답 body에 담아서 클라이언트에 반환
+    }
+
+    /**
+     * 특정 마감일에 해당하는 할일 목록을 반환
+     *
+     * @param dueDate 할일의 마감일
+     * @return 마감일에 해당하는 할일 목록
+     */
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks(Optional<String> dueDate) {
+        List<Task> result;
+
+        if (dueDate.isPresent()) {
+            result = this.taskService.getByDueDate(dueDate.get());
+        } else {
+            result = this.taskService.getAll();
+        }
+
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 특정 ID에 해당하는 할일을 조회
+     * @param id 할일 ID
+     * @return ID에 해당하는 할일 객체
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> fetchOneTask(@PathVariable Long id) {
+        var result = this.taskService.getOne(id);
+        return ResponseEntity.ok(result);
+    }
+
+    /**
+     * 특정 상태에 해당하는 할일 목록을 반환
+     * @param status 할일 상태
+     * @return 상태에 해당하는 할일 목록
+     */
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Task>> getByStatus(@PathVariable TaskStatus status) { //@PathVariable - {status} 경로 변수를 status로 바인딩
+        var result = this.taskService.getByStatus(status);
+        return ResponseEntity.ok(result);
     }
 }
